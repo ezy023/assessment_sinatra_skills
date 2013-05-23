b78c9be8ae1fed713091c91b1d9fb332e7a3cbbb
ezy023 <allareri@gmail.com>
2013-05-23 08:57:46 -0500

    Initial commit, full app push

---
+++ db/migrate/20130523082607_create_proficiencies_table.rb
@@ -0,0 +1,12 @@
+class CreateProficienciesTable < ActiveRecord::Migration
+  def change
+    create_table :proficiencies do |t|
+      t.integer :user_id
+      t.integer :skill_id
+      t.integer :years_of_education
+      t.boolean :formal_education
+      t.timestamps
+    end
+  end
+end
+
